2fb92220dcca3668f08d906e978d9296cb8ee889
Okpainmo <okpainmoandrew@gmail.com>
2022-05-06 01:09:13 +0100

    more progress

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import '../styles/css/main.module.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <meta
+          name="description"
+          content="hr-pro: an amazing hotel reservation practice-project 
+          built with next JS. Thanks to John Smilga and Coding-Addict"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
